3021d52c7c40e29cc496ab64976f9bc0c32218c2
SnowFlakeΨ <98556084+Snowflake-Coder-H2o@users.noreply.github.com>
2022-03-04 07:36:02 -0500

    Update inspect-element.js

--- inspect-element.js
+++ inspect-element.js
@@ -1,7 +1,7 @@
-//adds the inspect element (credit to snowlord)
+//adds the inspect element (credit to Snowflake-Coder-H2o)
 
 
   var x = document.createElement("script");
-  x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";
+  x.src = "https://cdn.jsdelivr.net/gh/Snowflake-Coder-H2o/devconsole@master/main.js";
   x.onload = alert("LoadedDeveloperConsole!");
   document.head.appendChild(x);
