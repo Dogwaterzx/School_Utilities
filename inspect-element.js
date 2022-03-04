926a8edd3c2173f199f6e28c7f5b5f78def05e7b
SnowFlakeΨ <98556084+Snowflake-Coder-H2o@users.noreply.github.com>
2022-03-04 07:38:25 -0500

    Delete inspect-element.js

--- inspect-element.js
+++
@@ -1,7 +0,0 @@
-//adds the inspect element (credit to snowlord)
-
-
-  var x = document.createElement("script");
-  x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";
-  x.onload = alert("LoadedDeveloperConsole!");
-  document.head.appendChild(x);
